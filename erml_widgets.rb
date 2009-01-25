e36b14f0495ff935c1071beff73e4633d17cdbcb
brent <brent@499c8170-6e11-0410-a2ac-93a16911f373>
2009-01-25 19:39:37 +0000

    ERML text encoding should always be UTF8. Test using classes for various languages' font encodings.
    git-svn-id: https://one.eidetic.us/svn/eidetic/trunk/libraries/eideticrml@983 499c8170-6e11-0410-a2ac-93a16911f373

--- erml_widgets.rb
+++ erml_widgets.rb
@@ -1577,7 +1577,7 @@ module EideticRML
 
       def print(writer)
         @document_page_no = 0
-        writer.open(:v_text_align => :base, :pages_up => pages_up, :pages_up_layout => pages_up_layout, :orientation => orientation)
+        writer.open(:v_text_align => :base, :pages_up => pages_up, :pages_up_layout => pages_up_layout, :orientation => orientation, :text_encoding => 'UTF8')
         pages.each do |page|
           page.print(writer)
         end
