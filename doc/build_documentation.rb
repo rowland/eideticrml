91468550a15dc44135aef6fa79c4d373875a5f95
brent <brent@499c8170-6e11-0410-a2ac-93a16911f373>
2009-05-04 03:47:12 +0000

    Placeholders for class documentation.
    git-svn-id: https://one.eidetic.us/svn/eidetic/trunk/libraries/eideticrml@1022 499c8170-6e11-0410-a2ac-93a16911f373

--- doc/build_documentation.rb
+++ doc/build_documentation.rb
@@ -10,24 +10,38 @@ require 'haml'
 require 'erml'
 
 class DocumentationBuilder
+  DEFAULT_EXTENSIONS = ['.erml.haml', '.erml']
+
   def render(filename)
-    @dirname ||= begin
-      if File.exist?(filename)
-        File.expand_path(File.dirname(filename))
-      else
-        File.expand_path(File.dirname(__FILE__))
-      end
-    end
+    dirname = File.dirname(filename)
     basename = File.basename(filename)
-    template = File.read(File.join(@dirname, basename))
-    Haml::Engine.new(template).render(binding)
+    Dir.chdir(dirname) do
+      template = File.read(fix_ext(basename))
+      Haml::Engine.new(template).render(binding)
+    end
+  end
+
+  def render_doc(filename)
+    dirname = File.dirname(filename)
+    basename = File.basename(filename).gsub(/\.(erml|haml)/,'')
+    pdfname = File.join(dirname, basename + '.pdf')
+    xml = render(filename)
+    pdf = EideticRML::XmlParser.parse(xml)
+    File.open(pdfname,'w') { |f| f.write(pdf) }
+    pdfname
+  end
+
+private
+  def fix_ext(filename)
+    DEFAULT_EXTENSIONS.each do |ext|
+      return filename + ext if File.exist?(filename + ext)
+    end
+    return filename
   end
 end
 
 filename = File.expand_path(ARGV.shift || File.join(File.dirname(__FILE__), 'EideticRML.erml.haml'))
-dirname = File.dirname(filename)
-pdfname = File.join(dirname, 'EideticRML.pdf')
-xml = DocumentationBuilder.new.render(filename)
-pdf = EideticRML::XmlParser.parse(xml)
-File.open(pdfname,'w') { |f| f.write(pdf) }
+builder = DocumentationBuilder.new
+pdfname = builder.render_doc(filename)
+
 `open #{pdfname}`
